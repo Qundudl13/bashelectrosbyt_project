9bfb21c6f1c114baf648ff63c68ee07e2d043b9b
SOLO-IMPERATOR <61757354+SOLO-IMPERATOR@users.noreply.github.com>
2023-08-07 14:36:23 +0300

    add image test

--- header.php
+++ header.php
@@ -8,4 +8,21 @@
     <link rel="stylesheet" href="style_main.css">
 </head>
 <body>
+    <header class="header">
+        <div class="header_wrapper">
+            <div class="header_upper">
+                <div class="header_upper_logo">
+                    <img src="" alt="" class="header_upper_logo_img">
+                </div>
+                <div class="header_upper_control">
+                    <div class="header_upper_control_find">
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="header_downer">
+
+            </div>
+        </div>
+    </header>
     <div class="wrapper">
