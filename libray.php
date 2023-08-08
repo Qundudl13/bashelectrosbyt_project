f29a40fdc4d9f19c9e618f64b75fedf039dc1bef
SOLO-IMPERATOR <61757354+SOLO-IMPERATOR@users.noreply.github.com>
2023-08-08 21:46:00 +0300

    add new html and css

---
+++ libray.php
@@ -0,0 +1,41 @@
+<?require("header.html")?> 
+<h1 class="main_title">
+    Библиотека
+</h1>
+<div class="libray_main_img">
+    <img src="/bashelectrosbyt_project/assets/img/libray_main.png" alt="" class="">
+</div>
+<div class="libray_text_wrapper">
+    <div class="libray_text">
+        Коллекция полезных материалов для обучения, которые помогут развиваться в профессии любому специалисту<br> и затрагивают темы универсальных навыков будущего.
+    </div>
+</div>
+
+<div class="library_list">
+    <div class="library_list_item">
+        <img src="/bashelectrosbyt_project/assets/img/tmp_alpi.png" alt="" class="library_list_item_img">
+        <a href="#" class="library_list_item_href">
+            Корпоративная библиотека Alpina Digital
+        </a>
+    </div>
+    <div class="library_list_item">
+        <img src="/bashelectrosbyt_project/assets/img/tmp_nrgy.png" alt="" class="library_list_item_img">
+        <a href="#" class="library_list_item_href">
+            Корпоративная газета «Энергия без границ»
+        </a>
+    </div>
+    <div class="library_list_item">
+        <img src="/bashelectrosbyt_project/assets/img/tmp_book.png" alt="" class="library_list_item_img">
+        <a href="#" class="library_list_item_href">
+            Библиотека инструментов по управлению изменениями
+        </a>
+    </div>
+    <div class="library_list_item">
+        <img src="/bashelectrosbyt_project/assets/img/tmp_link.png" alt="" class="library_list_item_img">
+        <a href="#" class="library_list_item_href">
+            Полезные ссылки
+        </a>
+    </div>
+</div>
+<?require("footer.html")?>
+
