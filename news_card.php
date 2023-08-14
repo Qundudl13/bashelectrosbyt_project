e2a4598c9c41cfc944b612304c4eab27b751d95d
SOLO-IMPERATOR <levchenko563@yandex.ru>
2023-08-14 21:38:45 +0300

    .

--- news_card.php
+++ news_card.php
@@ -1,58 +1,27 @@
-<!--<?require("header.php")?>-->
-<link rel = "stylesheet" type = "text/css" href = "style.css">
-<link rel = "stylesheet" type = "text/css" href = "style_main.css">
+<?require("header.php")?>
 
-<div class = "wrapper">
-    <p class = "path_to_page">Главная/Новости</p>
-    <div class = "news_content">
-        <div class = "news_content_text">
-            <? if() { /*NEWS WITH IMAGES*/ ?>
-                <div class = "news_content_text_img">
-                    <img src = "assets/img/tmp_news.png" alt>
-                </div>
-                <div class = "news_content_text_date">31.06.2023</div>
-                <div class = "main_title">График работы мобильного офиса</div>
-                <div class = "news_content_text_details">
-                    <p>Мобильный Офис «Башэлектросбыт» — это специально оборудованный автомобиль, в котором сотрудники «Башэлектросбыт» помогают клиентам решить свои вопросы.</p>
-                    <ul>
-                        <span>В мобильном офисе вы сможете:</span>
-                        <li>зарегистрироваться в «Личном кабинете»;</li>
-                        <li>оплатить квитанции за электроэнергию;</li>
-                        <li>заключить договор энергоснабжения;</li>
-                        <li>задать специалистам интересующие вас вопросы, касающиеся деятельности «Башэлектросбыта» по работе с физическими лицами;</li>
-                        <li>подать заявку на замену прибора учёта;</li>
-                        <li>получить счёт-извещение</li>
-                    </ul>
-                    <div class = "news_content_text_details_information">
-                        <div>СПРАВОЧНАЯ ИНФОРМАЦИЯ</div>
-                        <ul>
-                            <span>В июле офис принимает потребителей по следующему графику:</span>
-                            <li>06.07.2023 (четверг) с. Архангельское, возле почты РФ. Режим работы: 10:00-16:00;</li>
-                            <li>13.07.2023 (четверг) Белебеевский р-н, р.п. Приютово. Режим работы: 10:00-16:00;</li>
-                            <li>19.07.2023 (среда) Туймазинский район, с. Нижнетроицкий. Режим работы: 10:00-16:00;</li>
-                            <li>21.07.2023 (пятница) Дюртюлинский р-н, с.Семилетка. Режим работы: 10:00-16:00.</li>
-                        </ul>
-                    </div>
-                    <div class = "news_content_text_details_faq">Если возникнут вопросы, их всегда можно задать через Личный кабинет клиента «ЛКК ЭСКБ» или позвонив в Контактный центр по номерам 8-347-222-22-55, 8-347-222-22-00, 8-800-755-08-09.</div>
-                </div>
-
-            <? /* <-- NEWS WITH IMAGES*/ } else { /*NEWS WITHOUT IMAGES --> */ ?>
-                <div class = "news_content_text_img">
-                    <img src = "assets/img/tmp_news.png" alt>
-                </div>
-                <div class = "news_content_text_date">31.06.2023</div>
-                <div class = "main_title">График работы мобильного офиса</div>
-                <div class = "news_content_text_details">
-                    <p>Мобильный Офис «Башэлектросбыт» — это специально оборудованный автомобиль, в котором сотрудники «Башэлектросбыт» помогают клиентам решить свои вопросы.</p>
-                    <ul>
-                        <span>В мобильном офисе вы сможете:</span>
-                        <li>зарегистрироваться в «Личном кабинете»;</li>
-                        <li>оплатить квитанции за электроэнергию;</li>
-                        <li>заключить договор энергоснабжения;</li>
-                        <li>задать специалистам интересующие вас вопросы, касающиеся деятельности «Башэлектросбыта» по работе с физическими лицами;</li>
-                        <li>подать заявку на замену прибора учёта;</li>
-                        <li>получить счёт-извещение</li>
-                    </ul>
+<p class = "path_to_page">Главная/Новости</p>
+<div class = "news_content">
+    <div class = "news_content_text">
+        <? if(true) { /*NEWS WITH IMAGES*/ ?>
+            <div class = "news_content_text_img">
+                <img src = "assets/img/tmp_news.png" alt>
+            </div>
+            <div class = "news_content_text_date">31.06.2023</div>
+            <div class = "main_title">График работы мобильного офиса</div>
+            <div class = "news_content_text_details">
+                <p>Мобильный Офис «Башэлектросбыт» — это специально оборудованный автомобиль, в котором сотрудники «Башэлектросбыт» помогают клиентам решить свои вопросы.</p>
+                <ul>
+                    <span>В мобильном офисе вы сможете:</span>
+                    <li>зарегистрироваться в «Личном кабинете»;</li>
+                    <li>оплатить квитанции за электроэнергию;</li>
+                    <li>заключить договор энергоснабжения;</li>
+                    <li>задать специалистам интересующие вас вопросы, касающиеся деятельности «Башэлектросбыта» по работе с физическими лицами;</li>
+                    <li>подать заявку на замену прибора учёта;</li>
+                    <li>получить счёт-извещение</li>
+                </ul>
+                <div class = "news_content_text_details_information">
+                    <div>СПРАВОЧНАЯ ИНФОРМАЦИЯ</div>
                     <ul>
                         <span>В июле офис принимает потребителей по следующему графику:</span>
                         <li>06.07.2023 (четверг) с. Архангельское, возле почты РФ. Режим работы: 10:00-16:00;</li>
@@ -60,77 +29,101 @@
                         <li>19.07.2023 (среда) Туймазинский район, с. Нижнетроицкий. Режим работы: 10:00-16:00;</li>
                         <li>21.07.2023 (пятница) Дюртюлинский р-н, с.Семилетка. Режим работы: 10:00-16:00.</li>
                     </ul>
-                    <div class = "news_content_text_details_faq">Если возникнут вопросы, их всегда можно задать через Личный кабинет клиента «ЛКК ЭСКБ» или позвонив в Контактный центр по номерам 8-347-222-22-55, 8-347-222-22-00, 8-800-755-08-09.</div>
                 </div>
-            <? /*NEWS WITHOUT IMAGES*/} ?>
-            <div class = "news_content_text_links">
-                <div class = "news_content_text_links_item">
-                    <a href = "#">
-                        <img src = "assets/img/svg/social_network_1.svg" alt>
-                    </a>                   
-                </div>
-                <div class = "news_content_text_links_item">
-                    <a href = "#">
-                        <img src = "assets/img/svg/social_network_2.svg" alt>
-                    </a>
-                </div>
-                <div class = "news_content_text_links_item">
-                    <a href = "#">
-                        <img src = "assets/img/svg/social_network_3.svg" alt>
-                    </a>
-                </div>
-                <div class = "news_content_text_links_item">
-                    <a href = "#">
-                        <img src = "assets/img/svg/social_network_4.svg" alt>
-                    </a>
-                </div>
-                <div class = "news_content_text_links_item">
-                    <a href = "#">
-                        <img src = "assets/img/svg/social_network_5.svg" alt>
-                    </a>
-                </div>
-                <div class = "news_content_text_links_item">
-                    <a href = "#">
-                        <img src = "assets/img/svg/social_network_6.svg" alt>
-                    </a>
-                </div>
+                <div class = "news_content_text_details_faq">Если возникнут вопросы, их всегда можно задать через Личный кабинет клиента «ЛКК ЭСКБ» или позвонив в Контактный центр по номерам 8-347-222-22-55, 8-347-222-22-00, 8-800-755-08-09.</div>
             </div>
-            <div class = "news_toggle">
-                <div>Предыдущая новость</div>
-                <div>Следующая новость</div>
+
+        <? /* <-- NEWS WITH IMAGES*/ } else { /*NEWS WITHOUT IMAGES --> */ ?>
+            <div class = "news_content_text_date">31.06.2023</div>
+            <div class = "main_title">График работы мобильного офиса</div>
+            <div class = "news_content_text_details">
+                <p>Мобильный Офис «Башэлектросбыт» — это специально оборудованный автомобиль, в котором сотрудники «Башэлектросбыт» помогают клиентам решить свои вопросы.</p>
+                <ul>
+                    <span>В мобильном офисе вы сможете:</span>
+                    <li>зарегистрироваться в «Личном кабинете»;</li>
+                    <li>оплатить квитанции за электроэнергию;</li>
+                    <li>заключить договор энергоснабжения;</li>
+                    <li>задать специалистам интересующие вас вопросы, касающиеся деятельности «Башэлектросбыта» по работе с физическими лицами;</li>
+                    <li>подать заявку на замену прибора учёта;</li>
+                    <li>получить счёт-извещение</li>
+                </ul>
+                <ul>
+                    <span>В июле офис принимает потребителей по следующему графику:</span>
+                    <li>06.07.2023 (четверг) с. Архангельское, возле почты РФ. Режим работы: 10:00-16:00;</li>
+                    <li>13.07.2023 (четверг) Белебеевский р-н, р.п. Приютово. Режим работы: 10:00-16:00;</li>
+                    <li>19.07.2023 (среда) Туймазинский район, с. Нижнетроицкий. Режим работы: 10:00-16:00;</li>
+                    <li>21.07.2023 (пятница) Дюртюлинский р-н, с.Семилетка. Режим работы: 10:00-16:00.</li>
+                </ul>
+                <div class = "news_content_text_details_faq">Если возникнут вопросы, их всегда можно задать через Личный кабинет клиента «ЛКК ЭСКБ» или позвонив в Контактный центр по номерам 8-347-222-22-55, 8-347-222-22-00, 8-800-755-08-09.</div>
+            </div>
+        <? /*NEWS WITHOUT IMAGES*/} ?>
+        <div class = "news_content_text_links">
+            <div class = "news_content_text_links_item">
+                <a href = "#">
+                    <img src = "assets/img/svg/social_network_1.svg" alt>
+                </a>                   
+            </div>
+            <div class = "news_content_text_links_item">
+                <a href = "#">
+                    <img src = "assets/img/svg/social_network_2.svg" alt>
+                </a>
+            </div>
+            <div class = "news_content_text_links_item">
+                <a href = "#">
+                    <img src = "assets/img/svg/social_network_3.svg" alt>
+                </a>
+            </div>
+            <div class = "news_content_text_links_item">
+                <a href = "#">
+                    <img src = "assets/img/svg/social_network_4.svg" alt>
+                </a>
+            </div>
+            <div class = "news_content_text_links_item">
+                <a href = "#">
+                    <img src = "assets/img/svg/social_network_5.svg" alt>
+                </a>
+            </div>
+            <div class = "news_content_text_links_item">
+                <a href = "#">
+                    <img src = "assets/img/svg/social_network_6.svg" alt>
+                </a>
             </div>
         </div>
-        <aside class = "other_news">
-            <div class = "other_news_heading">Другие новости</div>
-            <div class = "other_news_content">
-                <div class = "other_news_content_block">
-                    <div>31.06.2023</div>
-                    <a href = "#">График работы мобильного офиса на июль</a>
-                </div>
-                <div class = "other_news_content_block">
-                    <div>31.06.2023</div>
-                    <a href = "#">График работы мобильного офиса на июль</a>
-                </div>
-                <div class = "other_news_content_block">
-                    <div>31.06.2023</div>
-                    <a href = "#">График работы мобильного офиса на июль</a>
-                </div>
-                <div class = "other_news_content_block">
-                    <div>31.06.2023</div>
-                    <a href = "#">График работы мобильного офиса на июль</a>
-                </div>
-                <div class = "other_news_content_block">
-                    <div>31.06.2023</div>
-                    <a href = "#">График работы мобильного офиса на июль</a>
-                </div>
-                <div class = "other_news_content_block">
-                    <div>31.06.2023</div>
-                    <a href = "#">График работы мобильного офиса на июль</a>
-                </div>
-            </div>
-        </aside>
+        <div class = "news_toggle">
+            <div>Предыдущая новость</div>
+            <div>Следующая новость</div>
+        </div>
     </div>
-    
+    <aside class = "other_news">
+        <div class = "other_news_heading">Другие новости</div>
+        <div class = "other_news_content">
+            <div class = "other_news_content_block">
+                <div>31.06.2023</div>
+                <a href = "#">График работы мобильного офиса на июль</a>
+            </div>
+            <div class = "other_news_content_block">
+                <div>31.06.2023</div>
+                <a href = "#">График работы мобильного офиса на июль</a>
+            </div>
+            <div class = "other_news_content_block">
+                <div>31.06.2023</div>
+                <a href = "#">График работы мобильного офиса на июль</a>
+            </div>
+            <div class = "other_news_content_block">
+                <div>31.06.2023</div>
+                <a href = "#">График работы мобильного офиса на июль</a>
+            </div>
+            <div class = "other_news_content_block">
+                <div>31.06.2023</div>
+                <a href = "#">График работы мобильного офиса на июль</a>
+            </div>
+            <div class = "other_news_content_block">
+                <div>31.06.2023</div>
+                <a href = "#">График работы мобильного офиса на июль</a>
+            </div>
+        </div>
+    </aside>
 </div>
+    
 
-<!-- <?require("footer.php")?> -->
+<?require("footer.php")?>
