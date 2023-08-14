03d3bd340e71d0d3724bd5fbf16d92e68f73a6f3
SOLO-IMPERATOR <61757354+SOLO-IMPERATOR@users.noreply.github.com>
2023-08-14 21:41:43 +0300

    1

---
+++ vacancy.php
@@ -0,0 +1,31 @@
+<?require("header.php")?> 
+<h1 class="main_title">
+    Вакансии
+</h1>
+
+<div class="list_vacancy">
+    <a href="#" class="list_vacancy_item">
+        <div class="list_vacancy_item_title">Ведущий специалист группы финансового контроля</div>
+        <div class="list_vacancy_item_desc">Обязанности: проверка корректности операций на счетах бухучета, контроль отрицательного сальдо, сбор информации, анализ ошибок, анализ синеттического и аналитического учета, мониторинг своевременного отражения операцияпо счетам.</div>
+    </a>
+    <a href="#" class="list_vacancy_item">
+        <div class="list_vacancy_item_title">Ведущий специалист группы финансового контроля</div>
+        <div class="list_vacancy_item_desc">Обязанности: проверка корректности операций на счетах бухучета, контроль отрицательного сальдо, сбор информации, анализ ошибок, анализ синеттического и аналитического учета, мониторинг своевременного отражения операцияпо счетам.</div>
+    </a>
+</div>
+
+<h2 class="vacancy_title">
+    Программа “Приведи друга”   
+</h2>
+
+<div class="invite">
+    <div class="invite_item">
+        <img src="" alt="" class="invite_item_img">
+        <div class="invite_item_text">
+            
+        </div>
+    </div>
+</div>
+
+<?require("footer.php")?>
+
