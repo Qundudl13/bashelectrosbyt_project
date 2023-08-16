609a998e0d2b622934cde22aad379c712b23baa8
Qundudl13 <yanovskiy.roma2017@gmail.com>
2023-08-16 20:35:37 +0300

    .

--- o_companii.php
+++ o_companii.php
@@ -1,19 +1,19 @@
 <?require("header.php")?>
-<h1 class = "main_title">О компании</h1>
-<div class = "information_company_links">
-    <a href = "/bashelectrosbyt_project/o_companii_struct.php">
-        <div class = "information_company_links_item">Организационная структура</div>
-    </a>
-    <a href = "#">
-        <div class = "information_company_links_item">История компании</div>
-    </a>
-    <a href = "/bashelectrosbyt_project/o_companii_corp_cultura.php">
-        <div class = "information_company_links_item">Корпоративная культура</div>
-    </a>
-    <a href = "/bashelectrosbyt_project/o_companii_results.php">
-        <div class = "information_company_links_item active">Результаты работы компании</div>
-    </a>
+    <h1 class = "main_title">О компании</h1>
+    <div class = "information_company_links">
+        <a href = "/bashelectrosbyt_project/o_companii_struct.php">
+            <div class = "information_company_links_item">Организационная структура</div>
+        </a>
+        <a href = "#">
+            <div class = "information_company_links_item">История компании</div>
+        </a>
+        <a href = "/bashelectrosbyt_project/o_companii_corp_cultura.php">
+            <div class = "information_company_links_item">Корпоративная культура</div>
+        </a>
+        <a href = "/bashelectrosbyt_project/o_companii_results.php">
+            <div class = "information_company_links_item active">Результаты работы компании</div>
+        </a>
 
-</div>
+    </div>
 
-<div class = "information_company_details">
+    <div class = "information_company_details">
