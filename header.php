4ffac43e154e53b3e7fe8a2fbcb2e665e4057000
SOLO-IMPERATOR <61757354+SOLO-IMPERATOR@users.noreply.github.com>
2023-08-16 17:43:57 +0300

    16.08

--- header.php
+++ header.php
@@ -29,34 +29,60 @@
                 </div>
                 <div class="header_downer">
                 <nav class="header_downer_nav">
+                    <div class="nav_element">
                     <a href="/bashelectrosbyt_project/o_companii_results.php" class="header_downer_nav_link">
                         <img src="/bashelectrosbyt_project/assets/img/svg/company.svg" alt="" class="header_downer_nav_link_img">
-                        <p class="header_downer_nav_link_text">О компании</p>
+                        <p class="header_downer_nav_link_text">О компании</p>  
                     </a>
-                    <a href="/bashelectrosbyt_project/sotrudinku.php" class="header_downer_nav_link">
-                        <img src="/bashelectrosbyt_project/assets/img/svg/worker.svg" alt="" class="header_downer_nav_link_img">
-                        <p class="header_downer_nav_link_text">Сотруднику</p>
-                    </a>
+                    <div class="subheader">
+                            <div class="subheader_element"><a href="#" class="subheader_a">Результаты работы компании</a>
+                        </div>
+                            <div class="subheader_element subheader_element_wth_child">
+                            <a href="#" class="subheader_a">Организационная структура</a>
+                            <div class="sub_subheader">
+                                    <div class="subheader_element"><a href="#" class="subheader_a">Результаты работы компании</a></div>
+                                    <div class="subheader_element"><a href="#" class="subheader_a">Результаты работы компании</a></div>
+                            </div>
+                        </div>
+                           
+                    </div>
+                    </div>
+                    <div class="nav_element">
+                        <a href="/bashelectrosbyt_project/sotrudinku.php" class="header_downer_nav_link">
+                            <img src="/bashelectrosbyt_project/assets/img/svg/worker.svg" alt="" class="header_downer_nav_link_img">
+                            <p class="header_downer_nav_link_text">Сотруднику</p>
+                        </a>
+                    </div>
+                    <div class="nav_element">
                     <a href="#" class="header_downer_nav_link">
                         <img src="/bashelectrosbyt_project/assets/img/svg/services.svg" alt="" class="header_downer_nav_link_img">
                         <p class="header_downer_nav_link_text">Служебные сервисы</p>
                     </a>
+                    </div>
+                    <div class="nav_element">
                     <a href="/bashelectrosbyt_project/news.php" class="header_downer_nav_link">
                         <img src="/bashelectrosbyt_project/assets/img/svg/news.svg" alt="" class="header_downer_nav_link_img">
                         <p class="header_downer_nav_link_text">Новости</p>
                     </a>
+                    </div>
+                    <div class="nav_element">
                     <a href="#" class="header_downer_nav_link">
                         <img src="/bashelectrosbyt_project/assets/img/svg/sprav.svg" alt="" class="header_downer_nav_link_img">
                         <p class="header_downer_nav_link_text">Справочная информация</p>
                     </a>
+                    </div>
+                    <div class="nav_element">
                     <a href="/bashelectrosbyt_project/libray.php" class="header_downer_nav_link">
                         <img src="/bashelectrosbyt_project/assets/img/svg/library.svg" alt="" class="header_downer_nav_link_img">
                         <p class="header_downer_nav_link_text">Библиотека</p>
                     </a>
+                    </div>
+                    <div class="nav_element">
                     <a href="/bashelectrosbyt_project/molodezh.php" class="header_downer_nav_link">
                         <img src="/bashelectrosbyt_project/assets/img/svg/activ.svg" alt="" class="header_downer_nav_link_img">
                         <p class="header_downer_nav_link_text">Молодежный актив</p>
                     </a>
+                    </div>
                 </nav>
             </div>
         </div>
--- vacancy.php
+++ vacancy.php
@@ -20,11 +20,46 @@
 
 <div class="invite">
     <div class="invite_item">
-        <img src="" alt="" class="invite_item_img">
+        <img src="assets/img/invite_1.png" alt="" class="invite_item_img">
         <div class="invite_item_text">
-            
+            Приведи друга на должность:
+            <ul>
+                <li>Специалист</li>
+                <li>Старший специалист</li>
+                <li>Юрисконсульт</li>
+            </ul>
         </div>
     </div>
+    <div class="invite_item">
+        <img src="assets/img/invite_2.png" alt="" class="invite_item_img">
+        <div class="invite_item_text">
+        Друг трудоустроился:
+            <ul>
+                <li>Прошел испытательный срок</li>
+                <li>Отработал не менее 6 месяцев в компании</li>
+            </ul>
+        </div>
+    </div>
+    <div class="invite_item">
+        <img src="assets/img/invite_3.png" alt="" class="invite_item_img">
+        <div class="invite_item_text">
+        Получи вознаграждение за рекомендацию, <br>
+        если друг продолжает работать в компании
+        </div>
+    </div>
+</div>
+<div class="vacancy_callback">
+По всем вопросам обращайся в группу корпоративного развития и сопровождения персонала,<br>
+вн.тел. 22-29 или на почтовый адрес.
+</div>
+<div class="vacancy_doc">
+    <a href="#" class="vacancy_doc_element">
+        <img src="assets/img/pdf_icon.png" alt="" class="vacancy_doc_element_img">
+        <div class="vacancy_doc_element_about">
+            <div class="vacancy_doc_element_about_title">Приказ ЭСКБ “ 346 от 21.06.2023</div>
+            <div class="vacancy_doc_element_about_typedoc">PDF, 6,4 МБ</div>
+        </div>
+    </a>
 </div>
 
 <?require("footer.php")?>
