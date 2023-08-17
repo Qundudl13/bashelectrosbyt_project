ea72682faa3302ea11bf8b0c48614812b02bff78
Qundudl13 <yanovskiy.roma2017@gmail.com>
2023-08-17 15:20:16 +0300

    Add new page

--- sprav_info.php
+++ sprav_info.php
@@ -2,33 +2,33 @@
 <h1 class = "main_title">Справочная информация</h1>
 <div class = "information_company_details">
     <div class = "information_company_details_item">
-        <div class = "sprav_info_details_item_wrapper">
-            <a href = "/bashelectrosbyt_project/">
+        <div class = "guide_info_details_item_wrapper">
+            <a href = "/bashelectrosbyt_project/sprav_info_sotrudnikov.php">
                 <div class = "information_company_details_item_link">Справочник сотрудников</div>
             </a>   
         </div>
-        <div class = "sprav_info_details_item_wrapper">
+        <div class = "guide_info_details_item_wrapper">
             <a href = "/bashelectrosbyt_project/">
                 <div class = "information_company_details_item_link">Справочник ВНД</div>
             </a> 
         </div>
-        <div class = "sprav_info_details_item_wrapper">
+        <div class = "guide_info_details_item_wrapper">
             <a href = "/bashelectrosbyt_project/">
                 <div class = "information_company_details_item_link">Образцы документов</div>
             </a>
         </div>
-        <div class = "sprav_info_details_item_wrapper">
+        <div class = "guide_info_details_item_wrapper">
             <a href = "/bashelectrosbyt_project/">
                 <div class = "information_company_details_item_link">Маршруты согласований</div>
             </a>
             
         </div>
-        <div class = "sprav_info_details_item_wrapper">
+        <div class = "guide_info_details_item_wrapper">
             <a href = "/bashelectrosbyt_project/">
                 <div class = "information_company_details_item_link">Инструкции</div>
             </a>
         </div>
-        <div class = "sprav_info_details_item_wrapper">
+        <div class = "guide_info_details_item_wrapper">
             <a href = "/bashelectrosbyt_project/">
                 <div class = "information_company_details_item_link">Корпоративный глоссарий</div>
             </a>
---
+++ sprav_info_sotrudnikov.php
@@ -0,0 +1,120 @@
+<?require("header.php")?>
+<link rel = "stylesheet" type = "text/css" href = "style_main.css">
+<link rel = "stylesheet" type = "text/css" href = "style.css">
+<p class = "path_to_page">Главная / Справочная информация</p>
+<h1 class = "main_title">Справочная информация</h1>
+<div class = "guide_info_staff">
+    <div class = "guide_info_staff_header">
+        <div class = "guide_info_staff_header_input">
+            <input type = "text" placeholder="Поиск по справочнику">
+        </div>
+        <div class = "guide_info_staff_header_links">
+            <div>
+                <a href = "#">Телефонный справочник ГК «Интер РАО</a>
+            </div>
+            <div>
+                <a href = "#">Телефонные коды выхода в «Интер РАО</a>
+            </div>
+        </div>
+    </div>
+    <div class = "guide_info_staff_content">
+        <table class = "guide_info_staff_content_table">
+            <tr class = "guide_info_staff_content_table_heading">
+                <th>Фотография</th>
+                <th>Ф.И.О.</th>
+                <th>E-mail</th>
+                <th>Должность</th>
+                <th>Подразделение</th>
+                <th>Общество</th>
+                <th>День рождения</th>
+                <th>Вн.телефон</th>
+                <th>Рабочее место</th>
+            </tr>
+            <tr class = "guide_info_staff_content_table_item">
+                <td><img src = "assets/img/tmp_guide_sotrudnikov.png" alt></td>
+                <td><p>Иванов Иван Иванович</p></td>
+                <td><a href = "#">ivanov@bashesk.ru</a></td>
+                <td>Руководитель</td>
+                <td>Руководство</td>
+                <td>ООО “ЭСКБ”</td>
+                <td>01.04.69</td>
+                <td>22-11</td>
+                <td>г.Уфа, ул.Менделеева, 132, каб.515</td>
+            </tr>
+            <tr class = "guide_info_staff_content_table_item">
+                <td><img src = "assets/img/tmp_guide_sotrudnikov.png" alt></td>
+                <td><p>Иванов Иван Иванович</p></td>
+                <td><a href = "#">ivanov@bashesk.ru</a></td>
+                <td>Руководитель</td>
+                <td>Руководство</td>
+                <td>ООО “ЭСКБ”</td>
+                <td>01.04.69</td>
+                <td>22-11</td>
+                <td>г.Уфа, ул.Менделеева, 132, каб.515</td>
+            </tr>
+            <tr class = "guide_info_staff_content_table_item">
+                <td><img src = "assets/img/tmp_guide_sotrudnikov.png" alt></td>
+                <td><p>Иванов Иван Иванович</p></td>
+                <td><a href = "#">ivanov@bashesk.ru</a></td>
+                <td>Руководитель</td>
+                <td>Руководство</td>
+                <td>ООО “ЭСКБ”</td>
+                <td>01.04.69</td>
+                <td>22-11</td>
+                <td>г.Уфа, ул.Менделеева, 132, каб.515</td>
+            </tr>
+            <tr class = "guide_info_staff_content_table_item">
+                <td><img src = "assets/img/tmp_guide_sotrudnikov.png" alt></td>
+                <td><p>Иванов Иван Иванович</p></td>
+                <td><a href = "#">ivanov@bashesk.ru</a></td>
+                <td>Руководитель</td>
+                <td>Руководство</td>
+                <td>ООО “ЭСКБ”</td>
+                <td>01.04.69</td>
+                <td>22-11</td>
+                <td>г.Уфа, ул.Менделеева, 132, каб.515</td>
+            </tr>
+        </table>
+    </div>
+</div>
+<div class = "guide_info_popup">
+    <div class = "guide_info_popup_content">
+        <div class = "guide_info_popup_content_img">
+            <img src = "assets/img/tmp_guide_popup.png" alt>
+        </div>
+        <div class = "guide_info_popup_content_text">
+            <div class = "guide_info_popup_content_text_name">Антонова Ольга Викторовна</div>
+            <div class = "guide_info_popup_content_text_property">
+                <div>Вн.телефон:</div>
+                <div>22-11</div>
+            </div>
+            <div class = "guide_info_popup_content_text_property">
+                <div>E-mail:</div>
+                <div>mail@bashesk.ru</div>
+            </div>
+            <div class = "guide_info_popup_content_text_property">
+                <div>Должность:</div>
+                <div>Менеджер</div>
+            </div>
+            <div class = "guide_info_popup_content_text_property">
+                <div>Подразделение:</div>
+                <div>Отдел продаж</div>
+            </div>
+            <div class = "guide_info_popup_content_text_property">
+                <div>Общество:</div>
+                <div>ООО “ЭСКБ”</div>
+            </div>
+            <div class = "guide_info_popup_content_text_property">
+                <div>День рождения</div>
+                <div>15.03.1980</div>
+            </div>
+            <div class = "guide_info_popup_content_text_property">
+                <div>Рабочее место:</div>
+                <div>г.Уфа, ул. Менделеева, 132,
+                    каб.515</div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?require("footer.php")?>
