f76db729ef7f0a6139003badff66638eae6e1340
Qundudl13 <yanovskiy.roma2017@gmail.com>
2023-08-17 13:15:21 +0300

    Add new page

---
+++ sprav_info.php
@@ -0,0 +1,39 @@
+<?require("header.php")?>
+<h1 class = "main_title">Справочная информация</h1>
+<div class = "information_company_details">
+    <div class = "information_company_details_item">
+        <div class = "sprav_info_details_item_wrapper">
+            <a href = "/bashelectrosbyt_project/">
+                <div class = "information_company_details_item_link">Справочник сотрудников</div>
+            </a>   
+        </div>
+        <div class = "sprav_info_details_item_wrapper">
+            <a href = "/bashelectrosbyt_project/">
+                <div class = "information_company_details_item_link">Справочник ВНД</div>
+            </a> 
+        </div>
+        <div class = "sprav_info_details_item_wrapper">
+            <a href = "/bashelectrosbyt_project/">
+                <div class = "information_company_details_item_link">Образцы документов</div>
+            </a>
+        </div>
+        <div class = "sprav_info_details_item_wrapper">
+            <a href = "/bashelectrosbyt_project/">
+                <div class = "information_company_details_item_link">Маршруты согласований</div>
+            </a>
+            
+        </div>
+        <div class = "sprav_info_details_item_wrapper">
+            <a href = "/bashelectrosbyt_project/">
+                <div class = "information_company_details_item_link">Инструкции</div>
+            </a>
+        </div>
+        <div class = "sprav_info_details_item_wrapper">
+            <a href = "/bashelectrosbyt_project/">
+                <div class = "information_company_details_item_link">Корпоративный глоссарий</div>
+            </a>
+        </div>
+    </div>  
+</div>  
+
+<?require("footer.php")?>
